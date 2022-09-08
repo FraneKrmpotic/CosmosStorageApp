9bf83f272e0e0741930fe361d3f4f150beb0852d
Frane Krmpotić <franekrmpotic@gmail.com>
2022-09-09 00:14:37 +0200

    commit

---
+++ CosmosStorage/CosmosStorage/Dao/CosmosDbServiceProvider.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Azure.Cosmos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CosmosStorage.Dao
+{
+    public static class CosmosDbServiceProvider
+    {
+        public const string DatabaseName = "Tasks";
+        public const string ContainerName = "Tasks";
+        public const string Account = "https://todoitems2.documents.azure.com:443/";
+        public const string Key = "pcEjba5l0arvwXLeWrGzGUsFb9jEEUgn2U2FZBKpK0BUpUZEnv1Uoo8ab9WLvoaMsBLdWECEr0bpXBuzGUElSQ==";
+
+
+        private static ICosmosDbService cosmosDbService;
+
+        public static ICosmosDbService CosmosDbService { get => cosmosDbService; }
+
+        public async static Task Init()
+        {
+            CosmosClient cosmosClient = new CosmosClient(Account, Key);
+            cosmosDbService = new CosmosDbService(cosmosClient, DatabaseName, ContainerName);
+            DatabaseResponse database = await cosmosClient.CreateDatabaseIfNotExistsAsync(DatabaseName);
+            await database.Database.CreateContainerIfNotExistsAsync(ContainerName, "/id");
+        }
+
+
+    }
+}
